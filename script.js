58d930bcf55ce5f79f4dd0782bb97e8ce1968004
noussayounsi <noussa.younsi@gmail.com>
2025-11-26 22:20:02 +0100

    gestion du clavier en jouant + le snake

--- script.js
+++ script.js
@@ -14,3 +14,16 @@ let food = {
 
 let direction = "RIGHT";
 let score = 0;
+document.addEventListener("keydown", event => {
+  if (event.key === "ArrowUp" && direction !== "DOWN") direction = "UP";
+  if (event.key === "ArrowDown" && direction !== "UP") direction = "DOWN";
+  if (event.key === "ArrowLeft" && direction !== "RIGHT") direction = "LEFT";
+  if (event.key === "ArrowRight" && direction !== "LEFT") direction = "RIGHT";
+});
+function draw() {
+  ctx.clearRect(0, 0, 400, 400);
+  snake.forEach(segment => {
+    ctx.fillStyle = "#0f0";
+    ctx.fillRect(segment.x, segment.y, box, box);
+  });
+}
