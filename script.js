250e1483f41cc66a61a9dd740425cd422b9df21d
noussayounsi <noussa.younsi@gmail.com>
2025-11-27 23:02:03 +0100

    javais un erreu jai pas ecrit defer dans le head pour que le js sexecute dans le canvas et jai termine donc linitialisation du food et snake dans le js avec quelques modification css et html

--- script.js
+++ script.js
@@ -14,16 +14,34 @@ let food = {
 
 let direction = "RIGHT";
 let score = 0;
+
 document.addEventListener("keydown", event => {
   if (event.key === "ArrowUp" && direction !== "DOWN") direction = "UP";
   if (event.key === "ArrowDown" && direction !== "UP") direction = "DOWN";
   if (event.key === "ArrowLeft" && direction !== "RIGHT") direction = "LEFT";
   if (event.key === "ArrowRight" && direction !== "LEFT") direction = "RIGHT";
 });
+
 function draw() {
   ctx.clearRect(0, 0, 400, 400);
+
   snake.forEach(segment => {
-    ctx.fillStyle = "#0f0";
+    ctx.fillStyle = "rgb(255, 0, 149)";
     ctx.fillRect(segment.x, segment.y, box, box);
   });
+
+  ctx.fillStyle = "yellow";
+  ctx.fillRect(food.x, food.y, box, box);
+
+  let head = { ...snake[0] };
+
+  if (direction === "UP") head.y -= box;
+  if (direction === "DOWN") head.y += box;
+  if (direction === "LEFT") head.x -= box;
+  if (direction === "RIGHT") head.x += box;
+
+  snake.unshift(head);
+  snake.pop();
 }
+
+setInterval(draw, 150);
